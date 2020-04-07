c7c476f742e765758093d665162b6000b6a4bc62
xinyuetang <16307110476@fudan.edu.cn>
2020-04-07 15:17:52 +0800

    init

---
+++ Blockly/src/test/java/com/advancedweb/Blockly/BlocklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advancedweb.Blockly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlocklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientJsonpModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientJsonpModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
